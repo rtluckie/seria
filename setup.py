a6350645df934fcd5368398628f65c5e7365b090
Ryan Luckie <rtluckie@gmail.com>
2015-10-25 15:13:10 -0500

    bump pypi version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_requires = [
 
 setup(
     name='seria',
-    version='0.1.3',
+    version='0.1.4',
     description='Serialization for Humans',
     long_description=long_description,
     author='Ryan Luckie',
